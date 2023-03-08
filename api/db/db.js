7021af39a190f672fa21bb83e2cad3d727e1ed2d
Raphael-Soares <soaresphaell@gmail.com>
2023-03-08 16:49:55 -0300

    fix: custom site head

--- api/db/db.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("dracdo", "root", "gamemode", {
-    dialect: "mysql",
-    host: "localhost",
-});
-
-module.exports = sequelize;
--- api/routes/Tasks.js
+++
@@ -1,6 +0,0 @@
-const api = require("express").Router();
-const Task = require("../models/Task");
-
-api.get("/", (req, res) => {
-    res.json({message: "Hello api"});
-});
